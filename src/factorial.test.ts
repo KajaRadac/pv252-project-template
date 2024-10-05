130c68487d5f53219141b5215e2539964603287a
Karolina Radačovská <553691@muni.cz>
2024-10-05 19:20:25 +0200

    Add unit tests to improve code coverage
    Signed-off-by: Karolina Radačovská <553691@muni.cz>

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -4,9 +4,20 @@ test("factorial-5", () => {
   expect(factorial(5)).toBe(120);
 });
 
+test("factorial-0", () => {
+  expect(factorial(0)).toBe(1);
+});
+
+test("factorial-floating-point", () => {
+  function will_throw() {
+    factorial(0.5);
+  }
+  expect(will_throw).toThrow("Negative numbers not supported");
+});
+
 test("factorial-minus", () => {
-  const will_throw = () => {
+  function will_throw() {
     factorial(-1);
-  };
+  }
   expect(will_throw).toThrow("Negative numbers not supported");
 });
--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -3,3 +3,21 @@ import { fibonacci } from "./fibonacci.ts";
 test("fibonacci-5", () => {
   expect(fibonacci(5)).toBe(5);
 });
+
+test("fibonacci-0", () => {
+  expect(fibonacci(0)).toBe(0);
+});
+
+test("fibonacci-floating-point", () => {
+  const will_throw = () => {
+    fibonacci(0.5);
+  };
+  expect(will_throw).toThrow("Cannot compute on negative numbers");
+});
+
+test("fibonacci-negative", () => {
+  const will_throw = () => {
+    fibonacci(-1);
+  };
+  expect(will_throw).toThrow("Cannot compute on negative numbers");
+});
